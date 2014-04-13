a4e3e47e69ed495a9d50dae8b99517ac6b4aabb4
Andrea Rojas <rojasmadero@gmail.com>
2014-04-13 20:50:55 +0200

    new test: player_wins_when_six_questions_are_answered_correctly
    Test to check that player wins when he answers correctly six questions

---
+++ tests/GameTests.java
@@ -0,0 +1,23 @@
+import static org.junit.Assert.*;
+import static org.hamcrest.Matchers.*;
+
+import org.junit.Test;
+
+import com.adaptionsoft.games.uglytrivia.Game;
+
+
+public class GameTests {
+
+	@Test
+	public void player_wins_when_six_questions_are_answered_correctly() {
+		boolean didWin = true;
+		Game aGame = new Game();
+		aGame.add("Rola");
+		
+		for(int i=0; i<6; i++){
+			didWin = aGame.wasCorrectlyAnswered();
+		}
+		assertThat(didWin,equalTo(false));
+	}
+
+}
